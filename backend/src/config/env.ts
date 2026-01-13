e5e5eadeeed8c862084b7765c1f503f93e929758
Aaditya Bobade <aadityabobade17@gmail.com>
2026-01-13 21:27:59 +0530

    Update env.ts

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,11 +3,7 @@ import { z } from "zod";
 
 const EnvSchema = z.object({
   PORT: z.string().default("5000"),
-  DB_HOST: z.string().default("localhost"),
-  DB_PORT: z.string().default("6450"),
-  DB_NAME: z.string().default("chat_app"),
-  DB_USER: z.string().default("postgres"),
-  DB_PASSWORD: z.string().default("postgres"),
+  DB_URL: z.string(),
   CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
 });
